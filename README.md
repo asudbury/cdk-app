4da290a7b58e77896c367b301ebbfbcae4e10b11
Adrian Sudbury <sudburya@gmail.com>
2024-06-08 18:29:02 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -12,3 +12,37 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+
+## CDK CLI Commands
+
+```bash
+# Install the AWS CDK CLI
+npm install -g aws-cdk
+
+# Initialize a new CDK project
+cdk init app --language=typescript
+
+# Bootstrap the AWS CDK Toolkit stack into an environment
+cdk bootstrap aws://ACCOUNT-NUMBER-1/REGION-1
+
+# Deploy the stack
+cdk deploy
+
+# Destroy the stack
+cdk destroy
+```
+
+## Useful Links
+
+* [AWS CDK Developer Guide](https://docs.aws.amazon.com/cdk/latest/guide/home.html)
+* [AWS CDK API Reference](https://docs.aws.amazon.com/cdk/api/latest/docs/aws-construct-library.html)
+
+
+
+
+
+
+
+
+
--- lib/cdk-app-stack.ts
+++ lib/cdk-app-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkAppStack extends cdk.Stack {
+/**
+ * Constructor for the CdkAppStack class.
+ *
+ * @param scope The parent Construct for this stack.
+ * @param id The logical ID of the stack.
+ * @param props (optional) Initialization properties for the stack.
+ */
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'CdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'CdkAppQueue', {
+       visibilityTimeout: cdk.Duration.seconds(300),
+       queueName: 'berty' // Set the name of the queue to 'berty'
+    });
   }
 }
